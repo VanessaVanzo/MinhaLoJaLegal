cc4962c9aae1d0d92782a58f55c1b15d01c2ec49
Vanessa Oliveira <nessavanzo@hotmail.com>
2025-08-21 20:38:06 -0300

    feat:adicioanandofiltro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { ProductService } from '../../service/product-service.service';
 import { CommonModule } from '@angular/common';
 import { CardProdutoComponent } from '../../components/card-produto-component/card-produto-component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,CardProdutoComponent,FlexLayoutModule],
+  imports: [CommonModule, CardProdutoComponent, FlexLayoutModule, MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  constructor(private productService: ProductService) { }
+
+  // recebe o signal do service
   products = this.productService.products;
 
-  constructor(private productService: ProductService) {}
+  // termo do filtro
+  filterTerm = signal('');
 
+  // computed: recalcula a lista filtrada sempre que mudar produtos() ou filterTerm()
+  filteredProducts = computed(() =>
+    this.products().filter(p =>
+      p.title.toLowerCase().includes(this.filterTerm().toLowerCase())
+    )
+  );
+
+  onFilterChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.filterTerm.set(input.value);
+  }
 
 
 }
